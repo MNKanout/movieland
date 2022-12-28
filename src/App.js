6e7ee66a10a93fb6e740ad47eeedd9790ccbc47e
Nour Kanout <mnkanout@gmail.com>
2022-12-28 23:06:39 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -8,14 +8,6 @@ import SearchIcon from './search.svg'
 
 const API_URL = 'https://omdbapi.com?apikey=561fef86';
 
-const movie1 = {
-    "Title": "Spiderman the Verse",
-    "Year": "2019–",
-    "imdbID": "tt12122034",
-    "Type": "series",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg"
-}
-
 const App = () => {
 
     const [movies,setMovies] = useState([]);
